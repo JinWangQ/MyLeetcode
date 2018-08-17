17c0f0fb4687d118c0a43237f731bbc22d080363
Jin <wangatqueens@gmail.com>
2018-08-17 17:52:55 -0400

    70ClimbingStairs

--- src/70ClimbingStairs/src.js
+++ src/70ClimbingStairs/src.js
@@ -3,6 +3,9 @@
  * @return {number}
  * a fibonacci problem
  * the number of way to get node n = (n-1) + (n-2)
+ * can also use array 
+ * n[0]=0; n[1]=1; n[2]=2;
+ * n[i] = n[i-1] + n[i-2];
  */
 var climbStairs = function(n) {
     //base cases
