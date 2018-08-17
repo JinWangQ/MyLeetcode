c4c44c261816db46e7e65ba4c34bbcc6fc9992b3
Jin <wangatqueens@gmail.com>
2018-08-17 17:48:42 -0400

    70ClimbingStairs

---
+++ src/70ClimbingStairs/src.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ * a fibonacci problem
+ * the number of way to get node n = (n-1) + (n-2)
+ */
+var climbStairs = function(n) {
+    //base cases
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    let res = 0,
+        one_step_before = 2,
+        two_steps_before = 1;
+
+    for (let i = 2; i < n; i++) {
+        res = one_step_before + two_steps_before;
+        two_steps_before = one_step_before;
+        one_step_before = res;
+    }
+    return res;
+};
